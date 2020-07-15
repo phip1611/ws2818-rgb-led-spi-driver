9a52860c77d552a62589b840e84ef9974c474b28
Philipp Schuster <phip1611@gmail.com>
2020-07-15 10:59:27 +0200

    fix

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -13,11 +13,11 @@ pub fn sleep_busy_waiting(ms: u64) {
 pub fn get_led_num_from_args() -> usize {
     println!(
         "You can provide the number of LEDs as argument when calling from command line.\
-        For example \"cargo run --bin moving_pixel 64\". The default is 64."
+        For example \"cargo run --bin <bin> 64\". The default is 64."
     );
     let args = std::env::args().collect::<Vec<String>>();
     if args.len() > 1 {
-        let leds = args[0].parse::<usize>();
+        let leds = args[1].parse::<usize>();
         if let Result::Ok(leds) = leds {
             println!("Using {} LEDs", leds);
             return leds;
@@ -33,11 +33,11 @@ pub fn get_led_num_from_args() -> usize {
 pub fn get_led_square_dim_from_args() -> usize {
     println!(
         "You can provide the number of LEDs as argument when calling from command line.\
-        For example \"cargo run --bin nxn-colored-display 8\" means 8x8=64 LEDs."
+        For example \"cargo run --bin <bin> 8\" means 8x8=64 LEDs."
     );
     let args = std::env::args().collect::<Vec<String>>();
     if args.len() > 1 {
-        let leds = args[0].parse::<usize>();
+        let leds = args[1].parse::<usize>();
         if let Result::Ok(leds) = leds {
             println!("Using {}x{} LEDs", leds, leds);
             return leds;
